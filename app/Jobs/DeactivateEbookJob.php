18a134af13ec5cef6283ce6e52d091e11d9d817e
Rogerio Pereira <hi@rogeriopereira.dev>
2025-11-17 14:47:23 -0500

    feat: Create Ebook Observers

---
+++ app/Jobs/DeactivateEbookJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Ebook;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class DeactivateEbookJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public Ebook $ebook
+    ) {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // Implementation will be done in Phase 2.4
+    }
+}
