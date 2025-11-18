e02f21c458b9cfacd10f9c7e66552d4539e17b6e
Rogerio Pereira <hi@rogeriopereira.dev>
2025-11-17 19:04:41 -0500

    refactor: Doesn't work on mautic 6

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,8 @@
 <?php
 
-use Triibo\Mautic\MauticServiceProvider;
-
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-    MauticServiceProvider::class,
 ];
